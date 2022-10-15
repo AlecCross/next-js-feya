545deddf97ce922f9f6d59f91aff9e9c2bd97929
AlecCross <cross2472@gmail.com>
2022-10-15 22:23:17 +0300

    Подключил apollo-client| Запрос списка товаров по названию подкатегории

--- pages/[id]/[subcategoryId].js
+++ pages/[id]/[subcategoryId].js
@@ -1,14 +1,19 @@
 import React from 'react'
 import PageContainer from '../../components/PageContainer'
 import db from "../../db.json"
+import { gql } from "@apollo/client";
+import client from "../../apollo-client";
 
 export default function Subcategory(props) {
+    console.log(props.apolloData)
     return <>
         <PageContainer header={props.subCategory.name}>
             <img
                 src={`${props.subCategory.imageUrl}`}
                 alt={`${props.subCategory.name}`}
             />
+            {Object.values(props.apolloData.categoryProducts).map(
+                product => <div key={product.id}>{product.name}</div>)}
         </PageContainer>
     </>
 }
@@ -33,9 +38,27 @@ export const getStaticPaths = async () => {
 
 export const getStaticProps = async (props) => {
     let test2 = {}
-    db.items.forEach(item => { if (item.id === props.params.id) item.subcategories.forEach(subCat => { if (subCat.subcategoryId === props.params.subcategoryId) test2 = subCat }) })
+    db.items.forEach(item => {
+        if (item.id === props.params.id)
+            item.subcategories.forEach(subCat => {
+                if (subCat.subcategoryId === props.params.subcategoryId) test2 = subCat
+            })
+    })
+    const { data } = await client.query({
+        query: gql`
+        query MyQuery($categoryName: [String]!) {
+            categoryProducts(categoryName: $categoryName) {
+                id
+                name
+            }
+        }
+        `, variables: { categoryName: test2.name }
+    })
+
+    console.log(data)
     return {
         props: {
+            apolloData: data,
             subCategory: test2
         }
     }
